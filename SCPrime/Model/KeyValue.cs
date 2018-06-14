3dfcbaf0dc7dc51be8f80966dff67d2f860083e2
longdq <longdq2902@gmail.com>
2018-06-14 21:01:19 +0700

    Commit file thieu

---
+++ SCPrime/Model/KeyValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCPrime.Model
+{
+    public class KeyValue
+    {
+        public int Key { get; set; }
+        public String Value { get; set; }
+
+        public KeyValue(int v, string v3)
+        {
+            this.Key = v;
+            this.Value = v3;
+        }
+
+ 
+
+       // public static List<KeyValue>
+    }
+}
