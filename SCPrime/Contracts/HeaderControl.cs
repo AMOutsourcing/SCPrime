da9145f3e3e717944af82af7b4889f4d3463c84a
longdq <longdq2902@gmail.com>
2018-07-15 21:37:55 +0700

    Change contract status v1

--- SCPrime/Contracts/HeaderControl.cs
+++ SCPrime/Contracts/HeaderControl.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SCPrime.Model;
+using SCPrime.Utils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,31 @@ namespace SCPrime.Contracts
 {
     public partial class HeaderControl : UserControl
     {
+        public delegate void SendStatus(string Message);
+        public SendStatus Sender;
+
         public HeaderControl()
         {
             InitializeComponent();
+
+            Sender = new SendStatus(GetStatus);
+        }
+
+        private void btnChangeStatus_Click(object sender, EventArgs e)
+        {
+            this.txtContractStatus.Text = "M";
+            ChangeStatusFrm sf = new ChangeStatusFrm();
+            sf.Sender(this.txtContractStatus.Text);
+            sf.ShowDialog();
+        }
+        private void GetStatus(string Message)
+        {
+            MessageBox.Show(Message);
+        }
+
+        private void HeaderControl_Load(object sender, EventArgs e)
+        {
+           
         }
     }
 }
